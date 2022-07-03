50f7571f756d3f103fc45588f77ef2e4ca6be1eb
Michael Zhao <michaelzhao314@gmail.com>
2022-07-02 23:39:31 -0500

    Initial Intellij Spring Boot template commit

---
+++ src/main/java/xyz/michaelzhao/javamagi/JavamagiApplication.java
@@ -0,0 +1,13 @@
+package xyz.michaelzhao.javamagi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavamagiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavamagiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/michaelzhao/javamagi/JavamagiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.michaelzhao.javamagi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavamagiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
